b336c100ed3fc215217b11b4afc2f9b6799580e2
Kalvin Nguyen <kalvin.win59@gmail.com>
2021-11-27 12:31:12 -0600

    order.js added get workig on post

--- server/routes/order.js
+++ server/routes/order.js
@@ -1,5 +1,12 @@
 const express = require("express");
 const database = require("../config/database");
 const router = express.Router();
+// grabs data and uses DESC to sort from highest value to lowest value order id 
+router.get("/", (req, res) => {
+    database
+        .query("SELECT * FROM orders WHERE is_open = true ORDER BY created_at DESC")
+        .then(data => res.status(200).json(data))
+        .catch(err => res.status(500).send({ error: err }));
+});
 
-
+router.post("/", (req, res) => {
