5e0d35dc1f341bf86c2ca31f4b440c04f04f7ba8
Kalvin Nguyen <kalvin.win59@gmail.com>
2021-11-27 23:27:35 -0600

    finished more of the routes and app.js. Nex tstep is to make validation for login

---
+++ server/routes/items.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const database = require("../config/database");
+const router = express.Router();
+
+// get route with error 
+router.get("/", (req, res) => {
+    database
+        .query("SELECT * FROM items")
+        .then(data => res.status(200).json(data))
+        .catch(err => res.status(500).send({ error: err }));
+});
+
+// get item by id 
+router.get("/:id", (req, res) => {
+    database
+        .query("SELECT * FROM items WHERE item_id = ?", [req.params.id])
+        .then(data => res.status(200).json(data))
+        .catch(err => res.status(500).send({ error: err }));
+});
+
+module.exports = router;
