de71d9582a9f492643b9e4d845a10e319675da25
Kalvin Nguyen <kalvin.win59@gmail.com>
2021-12-01 10:25:50 -0600

    added some actions to be referenced

---
+++ client/src/components/login/Login.js
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+
+// import the material strap or ui here 
+
+import classnames from "classnames";
+import { connect } from "react-redux";
+import { loginUser } from "../../actions/authActions";
