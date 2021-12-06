984ad44345708373c4ffb401bb4701abf212f864
Ronald Sit <mrronsit@gmail.com>
2021-12-05 21:59:31 -0600

    checkout drawer

--- client/src/index.js
+++ client/src/index.js
@@ -1,27 +1,39 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import { Provider } from 'react-redux';
+// import { createStore, applyMiddleware } from 'redux';
+// import { BrowserRouter as Router } from 'react-router-dom';
+// import { logger } from 'redux-logger'
+// import thunk from 'redux-thunk'
+// import reducers from './reducers';
+// import App from './components/App';
+// import * as serviceWorker from './serviceWorker'
+
+// const store = createStore(
+// 	reducers,
+// 	applyMiddleware(thunk, logger)
+// )
+
+// ReactDOM.render(
+// 	<Provider store={store}>
+// 		<Router>
+// 			<App />
+// 		</Router>
+// 	</Provider>, 
+// 	document.getElementById('root')
+// );
+
+
+// serviceWorker.register();
+
+//Ron's stuff YO KALVIN - You need any of the stuff above?
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-import { BrowserRouter as Router } from 'react-router-dom';
-import { logger } from 'redux-logger'
-import thunk from 'redux-thunk'
-import reducers from './reducers';
-import App from './components/App';
-import * as serviceWorker from './serviceWorker'
-
-const store = createStore(
-	reducers,
-	applyMiddleware(thunk, logger)
-)
+import './index.css';
+import NavTabs from './NavTabs';
+import Topbar from './Topbar';
+import Cards from './Cards';
 
 ReactDOM.render(
-	<Provider store={store}>
-		<Router>
-			<App />
-		</Router>
-	</Provider>, 
-	document.getElementById('root')
+<><Topbar/><NavTabs/><Cards/></>,document.getElementById('root')
 );
-
-
-serviceWorker.register();
